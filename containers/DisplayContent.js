b561726022808c97ddbd6ea1223c4cd061e92ed8
Anzhelika <angelika0094@gmail.com>
2018-11-12 16:51:06 +0200

    Add genres to display container

--- containers/DisplayContent.js
+++ containers/DisplayContent.js
@@ -2,20 +2,17 @@ import React from "react";
 import { connect } from "react-redux";
 import { handleRequestForTopRated, handleRequestForGenres } from "../actions";
 
-const getGeneresName = (movieGenreIds, genres) => {
+const getGenresName = (movieGenreIds, genres) => {
     let genresOfMovie = [];
-    let genresOfMovie2 = false;
     if (genres.genres) {
         for(let i=0; i < movieGenreIds.length; i++) {
             for(let j=0; j < genres.genres.length; j++) {
                 if (movieGenreIds[i] === genres.genres[j].id) {
                     genresOfMovie.push(genres.genres[j].name);
-                    genresOfMovie2 = true;
                 }
             }
         }
     }
-    console.error("action-getGenresName", genresOfMovie);
     return genresOfMovie;
 };
 
@@ -27,14 +24,14 @@ class DisplayContent extends React.Component {
     }
     render() {
         const listOfMovies = this.props.topRatedMovies.map((movie, index) => {
-            const generesName = getGeneresName(movie.genre_ids, this.props.genres);
-            const generes = generesName.map( (gener, index) => (
-                <p key={index}>{gener}</p>
+            const genresName = getGenresName(movie.genre_ids, this.props.genres);
+            const genres = genresName.map( (genre, index) => (
+                <p key={index}>{genre}</p>
             ));
             return (
                 <li key={index}>
                     <p>{movie.title}</p>
-                    <p>{generes}</p>
+                    <p>{genres}</p>
                 </li>
             )
         });
